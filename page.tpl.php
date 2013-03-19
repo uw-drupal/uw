61b9b5ab7ccf67ff26b89e69de9b43733dd902f1
Joel Walters <jtwalters@gmail.com>
2013-03-19 10:23:33 -0700

    Add messages and tabs into page.tpl.php

--- page.tpl.php
+++ page.tpl.php
@@ -166,22 +166,28 @@
 
 </header>
 
+<?php if ($main_menu || $secondary_menu): ?>
+  <!--div id="navigation"><div class="section">
+    <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
+    <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+  </div></div--> <!-- /.section, /#navigation -->
+<?php endif; ?>
 
-  <?php if ($main_menu || $secondary_menu): ?>
-    <!--div id="navigation"><div class="section">
-      <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-      <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-    </div></div--> <!-- /.section, /#navigation -->
-  <?php endif; ?>
+<?php if ($breadcrumb): ?>
+  <!--div id="breadcrumb"><?php print $breadcrumb; ?></div-->
+<?php endif; ?>
 
-  <?php if ($breadcrumb): ?>
-    <!--div id="breadcrumb"><?php print $breadcrumb; ?></div-->
-  <?php endif; ?>
+<?php print $messages; ?>
 
-  <?php //print $messages; ?>
+<div id="primary">
 
-  <div id="primary">
-    <div id="content" role="main" class="container">
+  <div id="tabs">
+    <?php if ($tabs): ?>
+      <div class="tabs"><?php print render($tabs); ?></div>
+    <?php endif; ?>
+  </div>
+
+  <div id="content" role="main" class="container">
 
     <div class="row show-grid">
       <div class="span8 column">
