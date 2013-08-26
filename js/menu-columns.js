1ecc98db831069b4728db4f88fcb73e7754c4343
Dan Druliner <seattledandy@gmail.com>
2013-08-26 15:13:11 -0700

    Added a column width property to fix overlapping columns in webkit browsers

--- js/menu-columns.js
+++ js/menu-columns.js
@@ -4,11 +4,12 @@
 */
 
 jQuery(document).ready(function($) {
-   var numitems = 9;  //The maximum number of items you want in each column of the dropdown
+   var numitems = 7;  //The maximum number of items you want in each column of the dropdown
    var dd_menu = $('div.navbar-inner > div > div > nav > div > section > ul.menu > li');  //the dropdowns object
    $.each( dd_menu , function( ){
        var colcount = '' + Math.ceil( $(this).find('ul>li').length / numitems );
-       $(this).find('ul').css( { 'column-count':colcount , '-moz-column-count':colcount ,  '--webkit-column-count':colcount  } );
+			 var colwidth = '' + 220 * colcount;
+       $(this).find('ul').css( { 'column-count':colcount , '-moz-column-count': colcount , '--webkit-column-count': colcount , 'width': colwidth + 'px'  } );
     });
   
 });
