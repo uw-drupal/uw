8aa8779b2905820a6fed0576437dce9510a3111b
Dan Druliner <seattledandy@gmail.com>
2013-08-28 16:39:20 -0700

    modify menu columns to work in webkit browsers

--- js/menu-columns.js
+++ js/menu-columns.js
@@ -1,16 +1,38 @@
 /* Create columns in the dropdown menu 
  * 
- * "numitems" variable lets you determine the max length of a menu before it starts creating columns.																																		 
+ * "numitems" variable lets you determine the max length of a menu before it starts creating columns.                                                                     
 */
 
 jQuery(document).ready(function($) {
-   var numitems = 7;  //The maximum number of items you want in each column of the dropdown
-   var dd_menu = $('div.navbar-inner > div > div > nav > div > section > ul.menu > li');  //the dropdowns object
-   $.each( dd_menu , function( ){
-       var colcount = '' + Math.ceil( $(this).find('ul>li').length / numitems );
-			 var colwidth = '' + 220 * colcount;
-       $(this).find('ul').css( { 'column-count':colcount , '-moz-column-count': colcount , '--webkit-column-count': colcount , 'width': colwidth + 'px'  } );
-    });
+  
+  columnify();
+  
+  function columnify(){
+                                  
+    var numitems = 9;  //The maximum number of items you want in each column of the dropdown
+    var dd_menu = $('div.navbar-inner > div > div > nav > div > section > ul.menu > li');  //the dropdowns object
+      $.each( dd_menu , function( ){
+        
+        if ($(window).width() > 760){  
+            var colcount = '' + Math.ceil( $(this).find('ul>li').length / numitems );
+            var colwidth = '' + 220 * colcount;
+    
+        }else{
+            var colcount = '' + 1;
+            var colwidth = '100%';
+        }
+        
+        $(this).find('ul').css( { 'column-count':colcount , '-moz-column-count': colcount , '--webkit-column-count': colcount , 'width': colwidth + 'px'  } );
+			  // $(this).find('ul').addClass( 'menu-column-' + colcount ); // tried this but it doesnt' seem to work in webkit.
+				
+				
+      });
+
+  }
+
+  $(window).resize(function() {
+    columnify();
+  });
   
 });
 
